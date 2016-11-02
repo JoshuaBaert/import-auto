11cb89177680720d5c5252949374d2e2b215a82c
Joshua Baert <jbghoring@gmail.com>
2016-11-02 17:24:36 -0600

    Mid-day save basic structure worked out

--- app/mainServc.js
+++ app/mainServc.js
@@ -4,6 +4,43 @@
 
 angular.module('main').service('mainServc', function ($http, $q) {
 	
+	var makes = [
+		{
+			make: 'volvo',
+			image: '../img/cars/volvo.jpg',
+			desc: [
+					'paragraph 1',
+					'paragraph 2',
+			],
+		}
+	];
+	
+	var hours = [
+		{
+			day: 'Monday',
+			hours: '8 AM - 5 PM'
+		},
+		{
+			day: 'Tuesday',
+			hours: '8 AM - 5 PM'
+		},
+		{
+			day: 'Wednesday',
+			hours: '8 AM - 5 PM'
+		},
+		{
+			day: 'Thursday',
+			hours: '8 AM - 5 PM'
+		},
+		{
+			day: 'Friday',
+			hours: '8 AM - 5 PM'
+		},
+		{
+			day: 'Saturday & Sunday',
+			hours: '8 AM - 5 PM'
+		},
+	]
 	
 	
 });
