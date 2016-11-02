b938228c29a4167a142d2185ec629ec637f8997d
Joshua Baert <jbghoring@gmail.com>
2016-11-02 15:59:57 -0600

    Mid-day save basic structure worked out

--- app/app.js
+++ app/app.js
@@ -2,4 +2,22 @@
  * Created by Joshua Baert on 10/28/2016.
  */
 
-angular.module('main', []);
+angular.module('main', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+	$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'views/home.html',
+			})
+			.state('restoration', {
+				url: '/restoration',
+				templateURL: 'views/restoration.html',
+			})
+			.state('services', {
+				url: 'services',
+				templateUrl: 'views/services.html',
+			})
+			.state('contact', {
+				url: '/contact',
+				templateUrl: 'views/contact.html'
+			})
+});
