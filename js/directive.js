c6d21c2f1b02810564e3e45d3eaa0643232ce617
Joshua Baert <jbghoring@gmail.com>
2016-11-07 18:10:10 -0700

    Fixed home image

--- js/directive.js
+++ js/directive.js
@@ -7,7 +7,7 @@ angular.module('main')
 			return {
 				restrict: 'A',
 				link: function (scope, element, attrs) {
-					element.css('background-image', 'url("img/cars/saab.jpg")');
+					element.css('background-image', 'url("https://github.com/JoshuaBaert/import-auto/blob/master/img/cars/Saab.jpg?raw=true")');
 					window.document.title = 'Home Provo Auto Repair'
 				}
 			}
